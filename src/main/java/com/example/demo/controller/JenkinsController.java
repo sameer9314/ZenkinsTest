f67363275432c5555d6a97bae609b6fb4cfaeba7
administrator <sameersaurabh9314@gmail.com>
2018-09-19 12:04:30 +0530

    first commit

---
+++ src/main/java/com/example/demo/controller/JenkinsController.java
@@ -0,0 +1,13 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class JenkinsController {
+	
+	@RequestMapping("/")
+	public String checkConnection() {
+		return "<html><body><h1 style=color:#a80128>Connection Stablished</h1></body></html>";
+	}
+}
